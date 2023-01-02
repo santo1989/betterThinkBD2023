177ff8c65461c8ab94107350eff89ff646bdc1cf
Jannatul Ferdows Modina <jannat.bubtcse@gmail.com>
2023-01-02 20:58:00 +0600

    db changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('uuid');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -30,6 +31,9 @@ class CreateUsersTable extends Migration
             $table->string('bank_name')->nullable();
             $table->string('branch')->nullable();
             $table->string('account_no')->nullable();
+            $table->boolean('is_approved_sponsor')->default(false);
+            $table->boolean('is_approved_payment')->default(false);
+
             $table->rememberToken();
             $table->timestamps();
         });
